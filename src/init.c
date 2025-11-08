84eceac1b8a532314cb98f9a2235087ac468bd3a
Francisco <frankwither991000@gmail.com>
2025-11-07 21:36:26 -0700

    Enhance init output and update system commands

--- src/init.c
+++ src/init.c
@@ -3,13 +3,14 @@
 #include <unistd.h>
 
 int main() {
-    printf(">>> CultraCore init iniciado <<<\n");
+    printf(">>> [INIT] CultraCore init iniciado <<<\n");
 
-    // Lanzar animación de arranque
-    system("system/bin/bootanim");
+    printf(">>> [INIT] Ejecutando bootanim...\n");
+    system("/bin/bootanim");
 
-    // Lanzar shell interactivo
-    system("system/bin/shell");
+    printf(">>> [INIT] Ejecutando shell...\n");
+    system("/bin/shell");
 
+    printf(">>> [INIT] Finalizando init.\n");
     return 0;
 }

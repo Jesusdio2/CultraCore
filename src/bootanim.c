9ec5df561454d1367f33137ebfd2168c5502d17d
Francisco <frankwither991000@gmail.com>
2025-11-07 21:35:41 -0700

    Add boot animation messages in Spanish

--- src/bootanim.c
+++ src/bootanim.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 
 int main() {
+    printf(">>> [BOOTANIM] Animación de arranque iniciada <<<\n");
+
     const char *frames[] = {
         "[=     ]",
         "[==    ]",
@@ -13,10 +15,15 @@ int main() {
     int num_frames = sizeof(frames) / sizeof(frames[0]);
 
     for (int i = 0; i < num_frames; i++) {
+        if (frames[i] == NULL) {
+            fprintf(stderr, ">>> [BOOTANIM] Error: frame %d no se pudo leer\n", i);
+            return 1;
+        }
         printf("\r%s", frames[i]);
         fflush(stdout);
         sleep(1);
     }
-    printf("\nBootanim terminado\n");
+
+    printf("\n>>> [BOOTANIM] Animación terminada <<<\n");
     return 0;
 }

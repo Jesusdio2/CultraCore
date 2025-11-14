1832ac557a2f650cf2f5faa35c7c5e79020e2f9f
Francisco <frankwither991000@gmail.com>
2025-11-14 15:00:10 -0700

    Update init.c

--- src/init.c
+++ src/init.c
@@ -5,11 +5,18 @@
 int main() {
     printf(">>> [INIT] CultraCore init iniciado <<<\n");
 
+    // Animación de arranque
     printf(">>> [INIT] Ejecutando bootanim...\n");
     system("/bin/bootanim");
 
-    printf(">>> [INIT] Ejecutando shell...\n");
-    system("/bin/shell");
+    // Intentar lanzar instalador gráfico
+    printf(">>> [INIT] Intentando ejecutar installer_gui...\n");
+    int rc = system("/bin/installer_gui");
+    if (rc != 0) {
+        printf(">>> [INIT] installer_gui falló o no está disponible (rc=%d).\n", rc);
+        printf(">>> [INIT] Ejecutando shell en su lugar...\n");
+        system("/bin/shell");
+    }
 
     printf(">>> [INIT] Finalizando init.\n");
     return 0;

13590a0e79b2a8b9057b82245e36d7b8217d88b1
Francisco <frankwither991000@gmail.com>
2025-11-18 21:11:34 -0700

    Update bootanim.c

--- src/bootanim.c
+++ src/bootanim.c
@@ -3,17 +3,23 @@
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *win = SDL_CreateWindow("CultraCore Boot", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 200, SDL_WINDOW_SHOWN);
+    SDL_Window *win = SDL_CreateWindow("CultraCore Boot",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 200, SDL_WINDOW_SHOWN);
     SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
     for (int i = 0; i <= 100; i += 5) {
         SDL_SetRenderDrawColor(ren, 18, 18, 18, 255); // fondo
         SDL_RenderClear(ren);
 
-        // barra
-        SDL_Rect bar = { 50, 100, i * 7, 30 };
+        // barra de progreso
+        SDL_Rect bar = { 50, 120, i * 7, 30 };
         SDL_SetRenderDrawColor(ren, 0, 145, 255, 255);
         SDL_RenderFillRect(ren, &bar);
+
+        // texto simulado
+        SDL_Rect text = { 50, 60, 700, 20 };
+        SDL_SetRenderDrawColor(ren, 180, 180, 180, 255);
+        SDL_RenderFillRect(ren, &text);
 
         SDL_RenderPresent(ren);
         usleep(150000); // animación suave
